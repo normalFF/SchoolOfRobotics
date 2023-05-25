171899ed417a58eb6b47de1ac300ee40c391f53d
fgvngfdf <fgvngfdf@gmail.com>
2023-05-25 18:56:33 +0300

    Добавлены зависимости

--- SchoolOfRobotics.Infrastructure/DependencyInjection.cs
+++ SchoolOfRobotics.Infrastructure/DependencyInjection.cs
@@ -1,9 +1,15 @@
 using Microsoft.Extensions.DependencyInjection;
+using SchoolOfRobotics.Application.Abstractions.Repositories;
+using SchoolOfRobotics.Infrastructure.Repositories;
 
 namespace SchoolOfRobotics.Infrastructure;
 
 public static class DependencyInjection {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
-        return services;
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IChildrenCollectionRepository, ChildrenCollectionRepository>();
+        services.AddScoped<IGroupRepository, GroupRepository>();
+        services.AddScoped<ICourseRepository, CourseRepository>();
+		return services;
     }
 }
